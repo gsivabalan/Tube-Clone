39c073f0beb3a21ab292d2159dcc67fdf43bbd9a
gsivabalan <siva90.balan@gmail.com>
2023-08-21 08:25:00 +0530

    second commit

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// Replace 'YOUR_API_KEY' with your actual YouTube API key
 const apiKey = 'AIzaSyAgpjV_EjXynIFZfcvKQXLNZouTVPq6CQ8';
 
-// Fetch videos from YouTube API
 function fetchVideos(query) {
     const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${query}&key=${apiKey}`;
 
@@ -9,18 +7,16 @@ function fetchVideos(query) {
         .then(response => response.json())
         .then(data => {
             console.log(data);
-            // Process the video data and generate thumbnails dynamically
+            
             const videos = data.items;
             const videoContainer = document.getElementById('video-container');
-            videoContainer.innerHTML = ''; // Clear previous video thumbnails
+            videoContainer.innerHTML = '';
 
             videos.forEach(video => {
                 const videoId = video.id.videoId;
                const channelid=video.id.channelId;
                 const videoTitle = video.snippet.title;
                 const videoThumbnail = video.snippet.thumbnails.medium.url;
-
-                // Create a thumbnail element
                 const thumbnailElement = document.createElement('div');
                 if(channelid!=null){
                     // https://www.googleapis.com/youtube/v3/channels?id=${channelid}&key=AIzaSyAgpjV_EjXynIFZfcvKQXLNZouTVPq6CQ8
@@ -44,12 +40,12 @@ function fetchVideos(query) {
         .catch(error => console.log(error));
 }
 
-// Handle form submission
+
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
 
 searchForm.addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the form from submitting
+    event.preventDefault(); 
 
     const searchQuery = searchInput.value;
     if (searchQuery !== '') {
@@ -57,5 +53,5 @@ searchForm.addEventListener('submit', function(event) {
     }
 });
 
-// Call the fetchVideos function to fetch and display default videos
+
 fetchVideos('tamil songs');
